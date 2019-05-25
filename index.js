5dcd95053a4f6b5aea7e833cc0da1fa647586569
David <digital0david@gmail.com>
2019-05-25 16:42:53 -0400

    get by id, delete

--- index.js
+++ index.js
@@ -45,6 +45,40 @@ router.get('/', (req, res) => {
     })
 });
 
+router.get('/:id', (req, res) => {
+  db('zoos')
+    .where({ id: req.params.id })
+    .then(zoo => {
+      res.status(200).json(zoo);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
+});
+
+router.delete('/:id', (req, res) => {
+  db('zoos')
+    .where({ id: req.params.id })
+    .del()
+    .then(zoo => {
+      if (!zoo) {
+        res.status(404).json({
+          message: "Zoo does not exist"
+        })
+        return;
+      } else {
+        res.status(200).json({
+          message: "Zoo has been deleted"
+        })
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
+})
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
