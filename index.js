19f1b18c90a9442f219764725145769c24c13baf
David <digital0david@gmail.com>
2019-05-25 14:17:42 -0400

    added post, does not return id

--- index.js
+++ index.js
@@ -2,22 +2,28 @@ const express = require('express');
 const helmet = require('helmet');
 const knex = require('knex');
 
+const router = require('express').Router();
 const knexConfig = require('./knexfile')
 const db = knex(knexConfig.development);
 
 const server = express();
 
 server.use(express.json());
+server.use('/api/zoos', router);
 server.use(helmet());
-
-const ZooRouter = require('express').Router();
-
-server.use('api/zoos', ZooRouter)
 
 // endpoints here
 
 router.post('/', (req, res) => {
-  db('')
+  
+  db.insert(req.body, 'id')
+    .into('zoos')
+    .then(res => {
+      res.status(201).json(res)
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
 })
 
 const port = 3300;
